8611fabecde98bb3d798a83b687d67d618234c96
Vlakosi <98867335+Vlakosi@users.noreply.github.com>
2022-02-24 11:52:27 +0600

    Add files via upload

---
+++ hh2/scrypt.js
@@ -0,0 +1,38 @@
+function str(a){
+    return console.log(a=String('one'))
+}
+str(1);
+const array1 = ['one', 'two', 'three'];
+console.log('array1:', array1);
+// expected output: "array1:" Array ["one", "two", "three"]
+
+const reversed = array1.reverse();
+console.log('reversed:', reversed);
+// expected output: "reversed:" Array ["three", "two", "one"]
+
+// Careful: reverse is destructive -- it changes the original array.
+console.log('array1:', array1);
+// expected output: "array1:" Array ["three", "two", "one"]
+
+// function min(a, b) {
+//     return console.log(a < b ? a : b);
+//   }
+//   min(432,10) 
+//   function pow(x, n) {
+//     let result = x;
+  
+//     for (let i = 1; i < n; i++) {
+//       result *= x;
+//     }
+  
+//     return result;
+//   }
+  
+//   let x = prompt("x?", '');
+//   let n = prompt("n?", '');
+  
+//   if (n < 1) {
+//     alert(`Степень ${n} не поддерживается, используйте натуральное число`);
+//   } else {
+//     alert( pow(x, n) );
+//   }
